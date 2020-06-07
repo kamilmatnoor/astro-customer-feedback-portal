79b05209d658805eaebfdf91ee51e9acd25ffa94
Kamil Noor <kamilmatnoor@gmail.com>
2020-06-07 22:03:01 +0800

    added query to add review by type

--- services/review/index.js
+++ services/review/index.js
@@ -25,8 +25,14 @@ const Review = (() => {
         new Promise(async (resolve, reject) => {
             let email = req.data.email
             let user_id = new Date().getTime()
+            let title = req.data.title
+            let technical = req.data.technical
+            let user = req.data.user
+            let management_related = req.data.management_related
+            let competitor_related = req.data.competitor_related
+            let pricing_issue = req.data.pricing_issue
+            let customer_case_issue = req.data.customer_case_issue
             let query = `insert into user (user_id, email) values ('${user_id}', '${email}');`
-            console.log(query)
             await db.query(query, (err, results) => {
                 if (err) {
                     reject({
@@ -35,16 +41,116 @@ const Review = (() => {
                     });
                     return
                 }
-
-                console.log(results)
             })
 
+            if (technical) {
+                query = `insert into review (user_id, review_type_id, title, description) values ('${user_id}', 1, '${title}', '${technical}');`
+                await db.query(query, (err, results) => {
+                    if (err) {
+                        reject({
+                            error: true,
+                            details: err
+                        });
+                        return
+                    }
+                })
+            }
+
+            if (user) {
+                query = `insert into review (user_id, review_type_id, title, description) values ('${user_id}', 2, '${title}', '${user}');`
+                await db.query(query, (err, results) => {
+                    if (err) {
+                        reject({
+                            error: true,
+                            details: err
+                        });
+                        return
+                    }
+                })
+            }
+
+            if (management_related) {
+                query = `insert into review (user_id, review_type_id, title, description) values ('${user_id}', 3, '${title}', '${management_related}');`
+                await db.query(query, (err, results) => {
+                    if (err) {
+                        reject({
+                            error: true,
+                            details: err
+                        });
+                        return
+                    }
+                })
+            }
+
+            if (competitor_related) {
+                query = `insert into review (user_id, review_type_id, title, description) values ('${user_id}', 4, '${title}', '${competitor_related}');`
+                await db.query(query, (err, results) => {
+                    if (err) {
+                        reject({
+                            error: true,
+                            details: err
+                        });
+                        return
+                    }
+                })
+            }
+
+            if (pricing_issue) {
+                query = `insert into review (user_id, review_type_id, title, description) values ('${user_id}', 5, '${title}', '${pricing_issue}');`
+                await db.query(query, (err, results) => {
+                    if (err) {
+                        reject({
+                            error: true,
+                            details: err
+                        });
+                        return
+                    }
+                })
+            }
+
+            if (customer_case_issue) {
+                query = `insert into review (user_id, review_type_id, title, description) values ('${user_id}', 6, '${title}', '${customer_case_issue}');`
+                await db.query(query, (err, results) => {
+                    if (err) {
+                        reject({
+                            error: true,
+                            details: err
+                        });
+                        return
+                    }
+                })
+            }
+
             resolve({ error: false })
-        
+
         })
+
+    const updateRating = req =>
+        new Promise((resolve, reject) => {
+            let total_like = req.data.total_like
+            let total_dislike = req.data.total_dislike
+            let review_id = req.data.review_id
+            
+            const query = `update review_rating set total_like = ${total_like}, total_dislike = ${total_like} where review_id = ${review_id}`
+
+            db.query(query, (err, results) => {
+                if (err) {
+                    reject({
+                        error: true,
+                        details: err
+                    });
+                    return
+                }
+
+                resolve({ error: false, data: results })
+
+            })
+        })
+
     return {
         getAll,
-        addNew
+        addNew,
+        updateRating
     }
 })()
 
