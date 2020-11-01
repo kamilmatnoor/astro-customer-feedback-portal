9c0b99212cfc70ff2d52d7dc2a762d671239ec99
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2020-11-01 10:54:34 +0800

    revised to improvised

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRouter)
 
 //load angular route
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 module.exports = app;
