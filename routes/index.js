51b63272a31acbc214fbe2e717cf0508f427c777
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2020-11-01 10:03:46 +0800

    revised to improvised

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ var router = express.Router()
 const UserFileRouter = require('./user')
 const ReviewFileRouter = require('./review/')
 const DashboardFileRouter = require('./dashboard/')
+const CaseDetailFileRouter = require('./case-detail/')
 
 router.use('/user', UserFileRouter)
 router.use('/review', ReviewFileRouter)
+
 router.use('/dashboard', DashboardFileRouter)
+router.use('/case-detail', CaseDetailFileRouter)
 
 module.exports = router
